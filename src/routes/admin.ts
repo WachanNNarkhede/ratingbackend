ee6cfc2dcefe8315a06d4b2eb2f5a186305dee7f
Wachan <109821254+WachanNNarkhede@users.noreply.github.com>
2025-08-22 15:51:36 +0530

    Update admin.ts

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import { submitRating, getUserRatings } from '../controllers/ratingController';
+import {getUsers} from '../controllers/adminController'
+import {getAllStores,getDashboardStats} from '../controllers/adminController'
+import {createUser} from '../controllers/adminController'
 import { authenticate, authorize } from '../middleware/auth';
 import { validateRating } from '../utils/validation';
 import { handleValidationErrors } from '../middleware/errorHandler';
 
 const router = Router();
 
-router.post('/', authenticate, authorize('NORMAL_USER'), validateRating, handleValidationErrors, submitRating);
-router.get('/my-ratings', authenticate, authorize('NORMAL_USER'), getUserRatings);
+router.post('/', authenticate, authorize('ADMIN'), validateRating, handleValidationErrors, submitRating);
+router.get('/dashboard', authenticate, authorize('ADMIN'), getUserRatings);
+router.get('/users', authenticate, authorize('ADMIN'), getUsers);
+
+router.post('/users', authenticate, authorize('ADMIN'), createUser);
+router.get('/stores', authenticate, authorize('ADMIN'), getAllStores);
+router.get('/dashboard', authenticate, authorize('ADMIN'), getDashboardStats);
+
 
 export default router;
